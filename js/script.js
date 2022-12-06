990db80ca88ff22c9119002307dcde65338163a8
Deh_Tarifa <denner_tarifa@outlook.com>
2022-12-05 23:35:46 -0300

    Atualização de interface
    Scroll da tela/ Animação

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // FUNCTION DROPDOWN MENU -------------------------*
 
 function show(a) {
-  document.querySelector('.nav_dropdown_option').value = a
+  document.querySelector('.input_dropdown_menu').value = a
 }
 
 let dropdown = document.querySelector('.header_dropdown_menu')
@@ -9,7 +9,7 @@ let dropdown = document.querySelector('.header_dropdown_menu')
   dropdown.classList.toggle('active')
 }
 
-// STYLE MODAL -----------------------------------*
+// STYLE P ABOUT ME -----------------------------------*
 
 function openVisual() {
   const aboutP = document.querySelector('.content_aboutme_p')
@@ -17,66 +17,13 @@ function openVisual() {
   aboutP.classList.toggle('p-active')
 }
 
-// FUNÇÃO SLIDER
+// SCROLL REVELATION -----------------------------------*
 
-class SlideStories {
-  constructor(id) {
-    this.slide = document.querySelector(`[data-slide="${id}"]`)
-    this.active = 0
-    this.init()
-  }
+window.sr = ScrollReveal({ reset: true});
 
-  activeSlide(index) {
-    this.active = index
-    this.items.forEach(item => item.classList.remove('active'))
-    this.items[index].classList.add('active')
-    this.thumbItems.forEach(item => item.classList.remove('active'))
-    this.thumbItems[index].classList.add('active')
-    this.autoSlide()
-  }
+sr.reveal('.defaut_section', { duration: 1500 })
+sr.reveal('.title_section', {
+  rotate: { x: 100, y: 0, z: 0},
+  duration: 1500,
+})
 
-  prev() {
-    if (this.active > 0) {
-      this.activeSlide(this.active - 1)
-    } else {
-      this.activeSlide(this.items.length - 1)
-    }
-  }
-
-  next() {
-    if (this.active < this.items.length - 1) {
-      this.activeSlide(this.active + 1)
-    } else {
-      this.activeSlide(0)
-    }
-  }
-
-  addNavigation() {
-    const nextBtn = this.slide.querySelector('.slide-next')
-    const prevBtn = this.slide.querySelector('.slide-prev')
-    nextBtn.addEventListener('click', this.next)
-    prevBtn.addEventListener('click', this.prev)
-  }
-
-  addThumbItems() {
-    this.items.forEach(() => (this.thumb.innerHTML += `<span></span>`))
-    this.thumbItems = Array.from(this.thumb.children)
-  }
-
-  autoSlide() {
-    clearTimeout(this.timeout)
-    this.timeout = setTimeout(this.next, 5000)
-  }
-
-  init() {
-    this.next = this.next.bind(this)
-    this.prev = this.prev.bind(this)
-    this.items = this.slide.querySelectorAll('.slide-items > *')
-    this.thumb = this.slide.querySelector('.slide-thumb')
-    this.addThumbItems()
-    this.activeSlide(0)
-    this.addNavigation()
-  }
-}
-
-new SlideStories('slide')
